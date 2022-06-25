81b548d1eb23248f5a6b00679368cf96c8a4f9d3
Komor <nurulkomor52@gmail.com>
2022-06-26 00:26:43 +0600

    text-selection

--- code/assets/js/main.js
+++ code/assets/js/main.js
@@ -40,8 +40,43 @@ delivery.onclick = () => {
 
 }
 
+// order section
+function add(btn_plus, btn_minus, display) {
+    var num = 0;
+    if (num >= 0) {
+        btn_plus.addEventListener('click', () => {
+            num++;
+            display.value = num;
+        })
+        if (num > -1) {
+            btn_minus.addEventListener('click', () => {
+                if (num > 0) {
+                    num--;
+                    display.value = num;
+                }
+            })
+        }
+    }
+}
+// One
+const btn_plus_one = document.querySelector("#chart-plus-one");
+const btn_minus_one = document.querySelector("#chart-minus-one");
+const display_one = document.querySelector("#add-input-one");
+add(btn_plus_one, btn_minus_one, display_one)
+// Two
+const btn_plus_two = document.querySelector("#chart-plus-two");
+const btn_minus_two = document.querySelector("#chart-minus-two");
+const display_two = document.querySelector("#add-input-two");
 
+add(btn_plus_two, btn_minus_two, display_two)
+// Three
+const btn_plus_three = document.querySelector("#chart-plus-three");
+const btn_minus_three = document.querySelector("#chart-minus-three");
+const display_three = document.querySelector("#add-input-three");
 
+add(btn_plus_three, btn_minus_three, display_three)
+
+// preloader
 $(window).on("load", function() {
     $(".preloader").fadeOut();
     $(".preloader").delay(5000).fadeOut("slow");
